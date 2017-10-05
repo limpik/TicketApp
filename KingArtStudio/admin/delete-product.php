35d04abeee65249de7994685f01643241b9cf167
Denis Dobuliak <limp2_96@mail.ru>
2017-10-05 20:46:11 +0300

    second commit

--- KingArtStudio/admin/delete-product.php
+++
@@ -1,6 +0,0 @@
-<?php
-   include("include/db_connect.php");
-$sql="DELETE FROM product WHERE id_product=$id"; 
-  mysql_query($sql); 
-
-?>
--- KingArtStudio/admin/delete_product.php
+++
@@ -1,9 +0,0 @@
-<?php
-   include("include/db_connect.php");
-   $id = $_GET['id_product'];
-   mysql_query(" DELETE FROM product WHERE id_product='$id' ");
-   mysql_close();
-header("Location: tovar.php");
-
-
-?>
